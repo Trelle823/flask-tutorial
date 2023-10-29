1871553fc575ed3df1742f0f32ceba765427cac7
Trelle823 <Trelle823@gmail.com>
2023-10-29 19:54:34 -0400

    Define auth blueprint routes

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,8 +1,25 @@
 import functools
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, seesion, url_for
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
